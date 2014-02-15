a82e82cd39274c2471c71c1550650e0e7d5e215d
Sacha Best <sachabest@outlook.com>
2014-02-14 19:31:59 -0500

    Adding a comment.

--- ParseiOSStarterProject/AppDelegate.cs
+++ ParseiOSStarterProject/AppDelegate.cs
@@ -21,9 +21,11 @@ namespace ParseiOSStarterProject
 
 		public AppDelegate ()
 		{
+			// this is a change
 			// Initialize the Parse client with your Application ID and .NET Key found on
 			// your Parse dashboard
-			ParseClient.Initialize("YOUR APPLICATION ID", "YOUR .NET KEY");
+			ParseClient.Initialize("iDAPKCcl35Edf2RIPBj2zN3ShBnXMapy8oQSPZWp",
+				"gbkBgqmKymSEWrccHQptP09PRy7GrSeF3prlXuG6");
 		}
 
 		//
